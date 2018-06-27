44188e4e804e60a99c3f8205044dc13d11064c46
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2018-06-27 22:27:55 +0100

    initial static server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,38 @@
 // server...
 console.log(`hello`)
+
+import * as express from 'express'
+import * as path from 'path'
+import * as http from 'http'
+//import * as bodyParser from 'body-parser'
+
+const app = express()
+
+// Parsers for POST data
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, '..', '..', 'visual-app', 'dist', 'visual-app')));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  //console.log(`get`)
+  res.sendFile(path.join(__dirname, '..', '..', 'visual-app', 'dist', 'visual-app', 'index.html'));
+})
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '8080'
+app.set('port', port)
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app)
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`))
